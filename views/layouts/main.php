59fc35524c8bd1132c87facc682ee0e82ae964c5
Anatoly Gusev <avg.tolik@gmail.com>
2015-04-14 19:32:08 +0300

    Update layout

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,70 +1,75 @@
 <?php
-  use yii\helpers\Html;
-  use yii\bootstrap\Nav;
-  use yii\bootstrap\NavBar;
-  use yii\widgets\Breadcrumbs;
-  use app\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
 
-  /* @var $this \yii\web\View */
-  /* @var $content string */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
-  AppAsset::register($this);
+AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-  <head>
-    <meta charset="<?= Yii::$app->charset ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-  </head>
-  <body>
-    <?php $this->beginBody() ?>
-    <?php $this->endBody() ?>
-    <div class="wrap">
+<head>
+  <meta charset="<?= Yii::$app->charset ?>"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?= Html::csrfMetaTags() ?>
+  <title><?= Html::encode($this->title) ?></title>
+  <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+<?php $this->endBody() ?>
+<div class="wrap">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
       <?php
-        NavBar::begin([
-          'brandLabel' => '',
-          'brandUrl'   => Yii::$app->homeUrl,
-          'options'    => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-          ],
-        ]);
+      NavBar::begin([
+        'brandLabel' => '',
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+          'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+        'renderInnerContainer' => false,
+      ]);
 
-        $items = [
-          ['label' => 'View Loads', 'url' => ['/site/index']],
-        ];
+      $items = [
+        ['label' => 'View Loads', 'url' => ['/site/index']],
+        ['label' => 'Yesterday\'s Loads', 'url' => ['/site/history']],
+      ];
 
-        if (!Yii::$app->user->isGuest) {
-          $items[] = ['label' => 'View All Loads', 'url' => ['/site/index', 'showCompleted' => 1]];
-          $items[] = ['label' => 'Customizations', 'url' => ['/site/config']];
-          $items[] = ['label' => 'Swi Spa Models', 'url' => ['/swim-spa/index']];
-          $items[] = ['label' => 'Hot Tub Models', 'url' => ['/hot-tub/index']];
-        }
+      if (!Yii::$app->user->isGuest) {
+        $items[] = ['label' => 'View All Loads', 'url' => ['/site/index', 'showCompleted' => 1]];
+        $items[] = ['label' => 'Customizations', 'url' => ['/site/config']];
+        $items[] = ['label' => 'Swi Spa Models', 'url' => ['/swim-spa/index']];
+        $items[] = ['label' => 'Hot Tub Models', 'url' => ['/hot-tub/index']];
+      }
 
-        $items[] = ['label' => 'New Load', 'url' => ['/load/create']];
-        $items[] = Yii::$app->user->isGuest ? ['label' => 'Login', 'url' => ['/site/login']] : ['label' => 'Logout (' . Yii::$app->user->identity->username . ')', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']];
+      $items[] = ['label' => 'New Load', 'url' => ['/load/create']];
+      $items[] = Yii::$app->user->isGuest ? ['label' => 'Login', 'url' => ['/site/login']] : ['label' => 'Logout (' . Yii::$app->user->identity->username . ')', 'url' => ['/site/logout'], 'linkOptions' => ['data-method' => 'post']];
 
-        echo Nav::widget([
-          'options' => ['class' => 'navbar-nav navbar-right'],
-          'items'   => $items,
-        ]);
-        NavBar::end();
-
-        echo Yii::$app->request->url;
+      echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => $items,
+      ]);
+      NavBar::end();
       ?>
-
-      <div class="container<?= Yii::$app->request->url == "/" || Yii::$app->request->url == "/site" ? "-fluid" : "" ?>">
-        <?= Breadcrumbs::widget([
-          'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= $content ?>
       </div>
     </div>
+  </div>
+  <div class="container<?= Yii::$app->request->url == "/" || Yii::$app->request->url == "/site" ? "-fluid" : "" ?>">
+    <?= Breadcrumbs::widget([
+      'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+    <?= $content ?>
+  </div>
+</div>
 
-    <!--footer class="footer">
+<!--footer class="footer">
       <div class="container">
         <p class="pull-left">&copy; AVG Company <?= date('Y') ?></p>
 
@@ -73,6 +78,6 @@
     </footer-->
 
 
-  </body>
+</body>
 </html>
 <?php $this->endPage() ?>
